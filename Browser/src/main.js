52baed441c67e2e4bd51c89d7bfe9a975fc3fe43
fujie <fujie@xiyoulinux.org>
2018-09-18 23:26:04 +0800

    塞入客户端

---
+++ Browser/src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+
+axios.defaults.withCredentials=true;//让ajax携带cookie
+
+Vue.prototype.$axios = axios;
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+});
